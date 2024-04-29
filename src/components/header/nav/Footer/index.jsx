0029ee31ef2de9470a031cab2f8360709940d05b
faaiq <66476388+verhaxity@users.noreply.github.com>
2024-04-29 22:39:43 +0530

    Add files via upload

---
+++ src/components/header/nav/Footer/index.jsx
@@ -0,0 +1,13 @@
+import styles from './style.module.scss';
+
+export default function index() {
+  return (
+    <div className={styles.footer}>
+        <a href= "mailto:faaiqkh124@gmail.com">Mail</a>
+        <a href='https://instagram.com/verhaxity/'>Instagram</a>
+        <a href='https://twitter.com/generativedata'>Twitter</a>
+        <a href='https://www.linkedin.com/in/faaiqmushtaq/'>LinkedIn</a>
+        <a href='https://www.github.com/verhaxity/'>Github</a>
+    </div>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,19 @@
+import floating1 from '../public/images/floating_1.png';
+import floating2 from '../public/images/floating_2.png';
+import floating3 from '../public/images/floating_3.png';
+import floating4 from '../public/images/floating_4.png';
+import floating5 from '../public/images/floating_5.png';
+import floating6 from '../public/images/floating_6.png';
+import floating7 from '../public/images/floating_7.png';
+import floating8 from '../public/images/floating_8.png';
+
+export { 
+    floating1, 
+    floating2, 
+    floating3, 
+    floating4, 
+    floating5, 
+    floating6, 
+    floating7, 
+    floating8
+}
